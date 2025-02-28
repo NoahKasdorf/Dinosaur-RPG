6e72a9569953b4a1ae210407af9399becd2f38a2
Rankinboy <noah.kasdorf@gmail.com>
2025-02-27 20:24:42 -0500

    Player and Entity class. Also worked on game loop.

--- main/GamePanel.java
+++ main/GamePanel.java
@@ -2,7 +2,12 @@ package main;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
 import javax.swing.JPanel;
+
+import entity.Player;
 
 public class GamePanel extends JPanel implements Runnable {
 
@@ -10,18 +15,26 @@ public class GamePanel extends JPanel implements Runnable {
     final int originalTileSize = 16; //16x16 tile
     final int scale = 3;
 
-    final int tileSize = originalTileSize * scale; //48x48 tile
+    public final int tileSize = originalTileSize * scale; //48x48 tile
     final int maxScreenCol = 16;
     final int maxScreenRow = 12;
     final int screenWidth = tileSize * maxScreenCol; //768 pixels
     final int screenHeight = tileSize * maxScreenRow; // 576 pixels
 
+    // FPS
+    int FPS = 60;
+
+    KeyHandler keyH = new KeyHandler();
     Thread gameThread;
+    Player player = new Player(this, keyH);
+
     
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight ));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
     }
 
     public void startGameThread() {
@@ -31,6 +44,57 @@ public class GamePanel extends JPanel implements Runnable {
 
     @Override
     public void run() {
+
+        double drawInterval = 1000000000 / FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+
+        long timer = 0;
+        int ticks = 0;
+        
+        while(gameThread != null) {
+           //ystem.out.println("Game is running");
+            
+            currentTime = System.nanoTime();
+
+            delta += (currentTime - lastTime) / drawInterval;
+
+            timer += currentTime - lastTime;
+
+            lastTime = currentTime;
+
+          if(delta >= 1) {
+                update();
+                repaint();
+                delta--;
+                ticks++;
+            }
+
+            if(timer >= 1000000000) {
+                System.out.println("FPS: " + ticks);
+                timer = 0;
+                ticks = 0;
+            }
+        }
+    }
+
+    public void update(){
+  
+        player.update();
+
+    }
+
+    // one of the standard methods to draw things on JPanel
+    public void paintComponent(Graphics g){
+
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D) g;
+
+        player.draw(g2);
+
+        g2.dispose();
     }
     
 }
